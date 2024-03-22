df2e9d5e334fa54a26dd6d316748e04859a56e05
Le Thanh Nhan <thanhnhan13082021@gmail.com>
2024-03-22 21:11:28 +0700

    Launch f8 project

---
+++ multi learning 2023/js_basics/lesson_1/main.js
@@ -0,0 +1,13 @@
+// var fullName = 'Le Thanh Nhan';
+// var age = 19;
+// alert(fullName)
+// alert(age)
+// var a = 5;
+// var b = 10;
+// if (a < b){
+//     alert("DUNG ROI")
+// }
+// else{
+//     alert("wrong")
+// }
+
